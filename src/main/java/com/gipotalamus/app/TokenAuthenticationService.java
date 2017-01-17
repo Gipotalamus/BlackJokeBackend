8ad66d564b201ec4bc42c7c4dd5b7efb9219752d
Gipotalamus <hectop23@ukr.net>
2017-01-17 23:02:02 +0200

    added configuration but not completed yet

---
+++ src/main/java/com/gipotalamus/app/TokenAuthenticationService.java
@@ -0,0 +1,7 @@
+package com.gipotalamus.app;
+
+/**
+ * Created by gipotalamus on 17.01.17.
+ */
+public class TokenAuthenticationService {
+}
---
+++ src/main/java/com/gipotalamus/app/controllers/UserC.java
@@ -0,0 +1,7 @@
+package com.gipotalamus.app.controllers;
+
+/**
+ * Created by gipotalamus on 17.01.17.
+ */
+public class UserC {
+}
